7b7ffe4b0c1bebb23c72713d1cc4341d54a0d3a4
warmflasha <warmflasha@gmail.com>
2019-10-03 12:27:50 -0500

    Added data

---
+++ inclass11.m
@@ -0,0 +1,17 @@
+%In this directory, you will find some example data, which is a 300x8
+%array stored in a .mat file. Image these are measurements for 300 genes are 8 different time
+%points
+
+%1. Use matlab to perform pca on this data, getting the transformation
+%matrix, the transformed data, and the eigenvalues.
+
+%2. Look at the eigenvalues - how many dimensions are necessary to explain
+%the data? 
+
+%3. Using kmeans clustering to cluster the data into 3 clusters. 
+% Make a plot of the first coordinate of the raw data vs the second color coded
+% by which cluster it is in. Make the same plot but use the first two
+% principle components instead. 
+
+%4. Repeat part 3 but use 7 clusters. Which is a more appropriate number of
+%clusters for the data?
